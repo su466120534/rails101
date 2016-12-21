559119cc1a481bdb2f17c41474c0ac467bf04334
sujiang <jimmysu1991@gmail.com>
2016-12-21 21:48:47 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,15 @@ Rails.application.routes.draw do
     member do
       post :join
      post :quit
+
   end
-  
+
    resources :posts
  end
+
+ namespace :account do
+   resources :groups
+ end
+ 
     root 'groups#index'
 end
